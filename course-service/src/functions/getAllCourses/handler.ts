9b24da8c8845240a346dad8f342da9fb6b332a98
Eugene Kovko <eu.kovko@gmail.com>
2022-09-23 14:55:48 +0200

    chore: Cleanup

--- course-service/src/functions/getAllCourses/handler.ts
+++ course-service/src/functions/getAllCourses/handler.ts
@@ -1,11 +1,15 @@
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
+import {formatJSONResponse} from '@libs/api-gateway';
+import {middyfy} from '@libs/lambda';
 
 import CourseRepository from "../../CourseRepositoryMock";
 
 const getAllCourses = async () => {
-  const repository = new CourseRepository()
-  const courses = repository.getAllCourses();
+  function getResult() {
+    const repository = new CourseRepository()
+    return repository.getAllCourses();
+  }
+
+  const courses = getResult();
   return formatJSONResponse({courses});
 };
 
--- course-service/src/functions/getCourseById/handler.ts
+++ course-service/src/functions/getCourseById/handler.ts
@@ -1,15 +1,19 @@
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
-import { APIGatewayProxyEvent } from "aws-lambda";
+import {formatJSONResponse} from '@libs/api-gateway';
+import {middyfy} from '@libs/lambda';
+import {APIGatewayEventDefaultAuthorizerContext, APIGatewayProxyEvent, APIGatewayProxyEventBase} from "aws-lambda";
 
 import CourseRepository from "../../CourseRepositoryMock";
 
 const getAllCourses = async (event: APIGatewayProxyEvent) => {
-  const repository = new CourseRepository()
-  const { idParam }  = event.pathParameters
-  const id = parseInt(idParam)
-  const course = repository.getCourseById(id);
+  const course = getResult(event);
   return formatJSONResponse({course});
 };
 
 export const main = middyfy(getAllCourses);
+
+function getResult(event: APIGatewayProxyEventBase<APIGatewayEventDefaultAuthorizerContext>) {
+  const repository = new CourseRepository()
+  const {idParam} = event.pathParameters
+  const id = parseInt(idParam)
+  return repository.getCourseById(id);
+}
